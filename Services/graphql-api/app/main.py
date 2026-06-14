cd07cfa081d8d9e164d8ad73f97f93053a7da520
anthony <anthony.coy@itmes.edu.gt>
2026-06-13 21:41:57 -0600

    prueba

--- Services/graphql-api/app/main.py
+++ Services/graphql-api/app/main.py
@@ -4,11 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API de GraphQL",
-    root_path="graphql",  
+    root_path="/graphql",
     docs_url="/docs",          
     openapi_url="/openapi.json"
 )
-cl
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
